e1c8bcbac02783633d964acfaaab94c3f333551e
Benedict <benedictryan80@gmail.com>
2022-06-18 00:26:42 +0700

    optimize the performance

--- components/Header.js
+++ components/Header.js
@@ -2,40 +2,40 @@ import React from 'react';
 import useCart from '../store/store';
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
+import shallow from 'zustand/shallow';
 
 const Header = () => {
-  const total = useCart((state) => state.total);
-  const totalqty = useCart((state) => state.totalqty);
+	const total = useCart((state) => state.total);
+	const totalqty = useCart((state) => state.totalqty);
 
-  const clearCart = useCart((state) => state.clearCart);
-  const [mytotal, setTotal] = useState();
-  const [mytotalqty, setTotalqty] = useState();
+	const [mytotal, setTotal] = useState();
+	const [mytotalqty, setTotalqty] = useState();
 
-  useEffect(() => {
-    setTotal(total);
-    setTotalqty(totalqty);
-  }, [total]);
-  return (
-    <div className="w-full bg-slate-200 py-4 ">
-      <div className="container mx-auto flex justify-between">
-        <div>
-          <Link href="/">
-            <a className="font-extrabold">MY-COOL-SHOP</a>
-          </Link>
-        </div>
-        <div className="font-light uppercase">
-          <Link href="/about">
-            <a>About</a>
-          </Link>
-          &nbsp; |&nbsp;
-          <Link href="/cart">
-            <a>Cart</a>
-          </Link>
-          : ${mytotal} / {mytotalqty}
-        </div>
-      </div>
-    </div>
-  );
+	useEffect(() => {
+		setTotal(total);
+		setTotalqty(totalqty);
+	}, [total]);
+	return (
+		<div className="w-full bg-slate-200 py-4 ">
+			<div className="container mx-auto flex justify-between">
+				<div>
+					<Link href="/">
+						<a className="font-extrabold">MY-COOL-SHOP</a>
+					</Link>
+				</div>
+				<div className="font-light uppercase">
+					<Link href="/about">
+						<a>About</a>
+					</Link>
+					&nbsp; |&nbsp;
+					<Link href="/cart">
+						<a>Cart</a>
+					</Link>
+					: ${mytotal} / {mytotalqty}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
--- pages/cart.js
+++ pages/cart.js
@@ -1,15 +1,18 @@
 import useCart from '../store/store';
 import Header from '../components/Header';
 import { useEffect, useState } from 'react';
+import shallow from 'zustand/shallow';
 
 export default function Home() {
-	const total = useCart((state) => state.total);
-	const cart = useCart((state) => state.cartContent);
-	const removeFromCart = useCart((state) => state.removeFromCart);
-
-	const reduceFromCart = useCart((state) => state.reduceFromCart);
-
-	const updatecart = useCart((state) => state.updatecart);
+	const { total, cart, removeFromCart, reduceFromCart } = useCart(
+		(state) => ({
+			total: state.total,
+			cart: state.cartContent,
+			removeFromCart: state.removeFromCart,
+			reduceFromCart: state.reduceFromCart
+		}),
+		shallow
+	);
 
 	const [mycart, setCart] = useState([]);
 	const [mytotal, setTotal] = useState();
@@ -30,53 +33,56 @@ export default function Home() {
 					<span className="basis-1/4 text-right uppercase font-bold">Qty</span>
 					<span className="basis-1/4 text-right uppercase font-bold"></span>
 				</div>
-				{mycart.map((item, key) => (
-					<div key={key} className="flex flex-row justify-between ">
-						<span className="basis-1/4">{item.name}</span>
-						<span className="basis-1/4 text-right">
-							$ {item.price * item.quantity}
-						</span>
-						<span className="basis-1/4 text-right">{item.quantity}</span>
-						<span className="basis-1/4 text-right">
-							<button
-								className="p-2 bg-slate-200 mr-7"
-								onClick={() => {
-									const product = mycart.findIndex((obj) => obj.id === item.id);
-									mycart[product].quantity--;
+				{mycart &&
+					mycart?.map((item, key) => (
+						<div key={key} className="flex flex-row justify-between ">
+							<span className="basis-1/4">{item.name}</span>
+							<span className="basis-1/4 text-right">
+								$ {item.price * item.quantity}
+							</span>
+							<span className="basis-1/4 text-right">{item.quantity}</span>
+							<span className="basis-1/4 text-right">
+								<button
+									className="p-2 bg-slate-200 mr-7"
+									onClick={() => {
+										const product = mycart.findIndex(
+											(obj) => obj.id === item.id
+										);
+										mycart[product].quantity--;
 
-									console.log(mycart[product].quantity);
+										console.log(mycart[product].quantity);
 
-									mycart[product].quantity < 1
-										? removeFromCart({
-												id: item.id,
-												price: item.price,
-												quantity: 1
-										  })
-										: reduceFromCart({
-												id: item.id,
-												price: parseInt(item.price),
-												quantity: item.quantity,
-												mycart
-										  });
-								}}
-							>
-								-
-							</button>
-							<button
-								className="p-2 bg-slate-200"
-								onClick={() =>
-									removeFromCart({
-										id: item.id,
-										price: item.price,
-										quantity: item.quantity
-									})
-								}
-							>
-								X
-							</button>
-						</span>
-					</div>
-				))}
+										mycart[product].quantity < 1
+											? removeFromCart({
+													id: item.id,
+													price: item.price,
+													quantity: 1
+											  })
+											: reduceFromCart({
+													id: item.id,
+													price: parseInt(item.price),
+													quantity: item.quantity,
+													mycart
+											  });
+									}}
+								>
+									-
+								</button>
+								<button
+									className="p-2 bg-slate-200"
+									onClick={() =>
+										removeFromCart({
+											id: item.id,
+											price: item.price,
+											quantity: item.quantity
+										})
+									}
+								>
+									X
+								</button>
+							</span>
+						</div>
+					))}
 				<div className="flex flex-row justify-between mt-4 border-t-2">
 					<span className="basis-full text-right uppercase font-bold">
 						Total: ${mytotal}
