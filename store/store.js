480da15eda6f4ae09ab185265ef443d6bdc85ff9
Benedict <benedictryan80@gmail.com>
2022-06-17 20:01:26 +0700

    first commit

---
+++ store/store.js
@@ -0,0 +1,45 @@
+import create from 'zustand';
+import { persist } from 'zustand/middleware';
+const useCart = create(
+	persist(
+		(set, get) => ({
+			total: 0,
+			totalqty: 0,
+			cartContent: [],
+			addTocart: (params) => {
+				set((state) => ({
+					totalqty: state.totalqty + 1,
+					total: state.total + parseFloat(params.price),
+					cartContent: [...state.cartContent, params]
+				}));
+			},
+			updatecart: ({ params, mycart }) => {
+				console.log(params);
+				set((state) => ({
+					totalqty: state.totalqty + 1,
+					total: state.total + parseFloat(params.price),
+					cartContent: mycart
+				}));
+			},
+			clearCart: () => set({ totalqty: 0, total: 0, cartContent: [] }),
+			removeFromCart: (params) => {
+				set(() => console.log(params));
+				set((state) => ({
+					total: state.total - params.price * params.quantity,
+					totalqty: state.totalqty - params.quantity,
+					cartContent: state.cartContent.filter((item) => item.id !== params.id)
+				}));
+			},
+			reduceFromCart: (params) => {
+				set(() => console.log(params));
+				set((state) => ({
+					totalqty: state.totalqty - 1,
+					total: state.total - params.price,
+					cartContent: params.mycart
+				}));
+			}
+		}),
+		{ name: 'cart' }
+	)
+);
+export default useCart;
