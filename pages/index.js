fb5e6d7da18c257ca150941a4438761a9076a216
Benedict <benedictryan80@gmail.com>
2022-06-18 00:38:34 +0700

    replace img with Image

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import useCart from '../store/store';
 import Header from '../components/Header';
 import { products } from '../lib/products';
 import shallow from 'zustand/shallow';
+import Image from 'next/image';
+
 export default function Home({ allProducts }) {
 	const { addTocart, updatecart, mycart } = useCart(
 		(state) => ({
@@ -47,10 +49,14 @@ export default function Home({ allProducts }) {
 								className="group"
 							>
 								<div className="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-									<img
+									<Image
 										src={product.imageSrc}
 										alt={product.imageAlt}
 										className="w-full h-full object-center object-cover group-hover:opacity-75"
+										// loading="eager"
+										priority
+										width={500}
+										height={500}
 									/>
 								</div>
 								<h3 className="mt-4 text-sm text-gray-700">{product.name}</h3>
